2366a0994d7662810b8a2375f7614f29460cb54f
AgustinSegui <agussegui24@hotmail.es>
2024-09-15 22:58:07 -0300

    feat: add RootLayout with Raleway font and LoadingScreenWithButton component
    - Implemented RootLayout component with Raleway font, including multiple weights and styles.
    - Configured metadata with title and description for the site Pan y Circo.
    - Added a loading screen component LoadingScreenWithButton to simulate a store loading experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,58 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function LoadingScreenWithButton() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [title, setTitle] = useState("Cargando Tu Tienda...")
+
+  useEffect(() => {
+    // Simulamos un tiempo de carga de 3 segundos para los datos
+    setTimeout(() => {
+      setLoading(false);
+      setTitle("Tu Tienda está lista!")
+    }, 3000); // 3 segundos
+  }, []);
+
+  const handleClick = () => {
+    router.push("/order/cafe"); // Redirecciona a la página de la tienda
+  };
+
   return (
-    <h1>Hi there</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-white">
+      <header className="text-center mb-8">
+        <div className="">
+          <Image 
+            src="/logo.svg"
+            width={300}
+            height={300}
+            alt="Tienda Logo"
+            className="mb-4 flex items-center"
+          />
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+      </header>
+
+      {loading ? (
+        <>
+          <p className="text-lg text-gray-700">Preparando tus productos...</p>
+          <div className="mt-8 flex justify-center items-center space-x-2">
+            <div className="w-4 h-4 bg-blue-500 rounded-full animate-bounce"></div>
+            <div className="w-4 h-4 bg-green-500 rounded-full animate-bounce delay-100"></div>
+            <div className="w-4 h-4 bg-red-500 rounded-full animate-bounce delay-200"></div>
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={handleClick}
+          className="mt-8 bg-yellow-500 text-black py-3 px-6 rounded-lg hover:bg-yellow-600 transition duration-300"
+        >
+          Empezar Compras
+        </button>
+      )}
+    </div>
   );
 }
